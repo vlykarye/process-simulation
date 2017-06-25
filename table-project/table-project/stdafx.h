ef894e297167ed120b802f52e39fbe60bc7c08e1
eric <vlykarye@gmail.com>
2017-06-25 18:26:39 -0500

    first commit
    added .gitignore file
    added code I have thus far

---
+++ table-project/table-project/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <iostream>                           // several standard stream objects
+#include <string>                             // std::basic_string class template
+#include <sstream>                            // std::basic_stringstream, std::basic_istringstream, std::basic_ostringstream class templates and several typedefs
+#include <fstream>                            // std::basic_fstream, std::basic_ifstream, std::basic_ofstream class templates and several typedefs
+#include <map>                                // std::map and std::multimap associative containers
+#include <queue>                              // std::queue and std::priority_queue container adaptors
+
+// TODO: reference additional headers your program requires here
