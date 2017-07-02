e382e9e3b031cf20ebb2be9a405df7af322a6609
vlykarye <vlykarye@gmail.com>
2017-07-01 19:19:47 -0500

    machine constructor and process table print function

--- table-project/table-project--2/main.cpp
+++ table-project/table-project--2/main.cpp
@@ -20,11 +20,12 @@ TO to(FROM const & a)
 
 namespace project
 {
-     string const TASK_STRING[]{ "NCORES", "NEW", "CORE", "DISK", "DISPLAY", "INPUT", "TERMINATE" };
+     string const TASK_STRING[]{ "NCORES", "NEW", "CORE", "DISK", "DISPLAY", "INPUT", "TERMINATE", "NONE" };
      enum struct TASK { NCORES, NEW, CORE, DISK, DISPLAY, INPUT, TERMINATE, NONE };
+     string const STATE_STRING[]{ "RUNNING", "READY", "BLOCKED", "NONE" };
      enum struct STATE { RUNNING, READY, BLOCKED, NONE };
 
-
+     /// DONE
      // { id, end_time }
      struct
           resource
@@ -32,6 +33,7 @@ namespace project
           int id;    int end_time;
      };
 
+     /// DONE
      // { request, value }
      struct
           task
@@ -39,21 +41,30 @@ namespace project
           TASK request;    int value;
      };
 
+     /// DONE
      // { id, tasklist, start_time, end_time, status }
      struct
           process
      {
           process(queue<task> & new_tasklist)
-               : tasklist(new_tasklist) {}
+               : tasklist(new_tasklist)
+          {
+               id = 0;
+               start_time = 0;
+               end_time = 0;
+               status = STATE::NONE;
+          }
 
           int id;    queue<task> tasklist;    int start_time;    int end_time;    STATE status;
      };
 
-
+     /// DONE
      class
           resource_table
      {
      public:
+          resource_table() {}
+
           resource_table(int count)
                : resources(count, resource{ -1, -1 }) {}
 
@@ -106,290 +117,29 @@ namespace project
           process_table
      {
      public:
+          process_table() {}
+
           process_table(vector<process> && new_processes)
                : processes(new_processes) {}
-
-     private:
-          vector<process> processes;
-     };
-
-     class
-          machine
-     {
-     public:
-
-     private:
-          int system_time = 0;
-     };
-
-
-     /*
-     class
-          tasklist
-     {
-     public:
-          tasklist(vector<task> && list_of_tasks)
-          {
-               // a comparator function similar to the one in struct task that takes
-               // into consideration the 'id', as the 'order' is dependent upon it
-               // up until this point
-               struct temp_comparator
-               {
-                    bool operator()(task const & a, task const & b) const
-                    {
-                         if ( a.start_time == b.start_time )
-                         {
-                              if ( a.id == b.id )
-                              {
-                                   return (a.order > b.order);
-                              }
-                              return (a.id > b.id);
-                         }
-                         return (a.start_time > b.start_time);
-                    }
-               };
-
-               // a temporary priority queue that utilizes the temperary comparator
-               // function defined above
-               priority_queue<task, vector<task>, temp_comparator> temp;
-
-               // firstly, the tasks must taken from the vector and pushed into the
-               // temporary priority queue defined above
-               auto && beg = list_of_tasks.begin() + 1;
-               auto && end = list_of_tasks.end();
-               for ( ; beg != end; ++beg )
-               {
-                    auto && r = *beg;
-                    temp.push(r);
-               }
-
-               // next, now that the tasks are properly sorted, they must be copied
-               // from the temprory priority queue into the priority queue of this
-               // tasklist object
-               task p{ -1, NONE, -1, false, -1, -1, -1 };
-               while ( temp.empty() == false )
-               {
-                    task t = temp.top();
-                    temp.pop();
-
-                    t.order = 0;
-                    if ( t.start_time == p.start_time ) {
-                         t.order = p.order + 1;
-                    }
-                    p = t;
-                    tasks.push(t);
-               }
-          }
-
-          void
-               execute()
-          {
-               int number_of_cores = tasks.top().value;
-
-               resource_table core(number_of_cores);
-               resource_table disk(1);
-
-               queue<task> completed_tasks;
-
-               int count = 0;
-               while ( tasks.empty() == false )
-               {
-                    count++;
-                    task t = tasks.top();
-                    tasks.pop();
-
-                    switch ( t.request )
-                    {
-                         case NEW:
-                         {
-                              completed_tasks.push(t);
-                         }
-                         break;
-
-                         case CORE:
-                         {
-                              int next_free_time = 0;
-                              if ( core.request(t, next_free_time) == true )
-                              {
-                                   completed_tasks.push(t);
-                              }
-                              else
-                              {
-                                   cout << "core: " << next_free_time << endl;
-                              }
-                         }
-                         break;
-
-                         case DISK:
-                         {
-                              int next_free_time = 0;
-                              if ( core.request(t, next_free_time) == true )
-                              {
-                                   completed_tasks.push(t);
-                              }
-                              else
-                              {
-                                   cout << "disk: " << next_free_time << endl;
-                              }
-                         }
-                         break;
-
-                         case DISPLAY:
-                         {
-                              completed_tasks.push(t);
-                         }
-                         break;
-
-                         case INPUT:
-                         {
-                              completed_tasks.push(t);
-                         }
-                         break;
-
-                         case TERMINATE:
-                         {
-
-                         }
-                         break;
-                    }
-               }
-
-               print(completed_tasks);
-          }
 
           void
                print()
           {
-               auto copy(tasks);
-               while ( copy.empty() == false )
+               for ( auto & p : processes )
                {
-                    task const & t = copy.top();
-                    cout << setw(3) << t.id << " " << setw(10) << left << TASK_STRING[t.request] << " " << setw(5) << right << t.value << " " << setw(5) << t.start_time << " " << setw(5) << t.end_time << " " << t.blocking << " " << t.order << endl;
-                    copy.pop();
-               }
-               cout << endl;
-          }
-
-          void
-               print(queue<task> list)
-          {
-               auto copy(list);
-               int count = 0;
-               while ( copy.empty() == false )
-               {
-                    count++;
-                    task const & t = copy.front();
-                    cout << setw(3) << t.id << " " << setw(10) << left << TASK_STRING[t.request] << " " << setw(5) << right << t.value << " " << setw(5) << t.start_time << " " << setw(5) << t.end_time << " " << t.blocking << " " << t.order << endl;
-                    copy.pop();
-               }
-               cout << endl;
-               cout << count << endl;
-          }
-
-
-
-          bool
-               validate()
-          {
-               // iterate the tasklist checking that the values of each task compared
-               // to the previous task do not violate these rules:
-               // 1)
-
-               // task { id, request, value, blocking, start_time, end_time, order }
-               task p{ -1, NONE, -1, false, -1, -1, -1 };
-
-               auto copy(tasks);
-               while ( copy.empty() == false )
-               {
-                    task const & t = copy.top();
-                    if ( t.start_time > t.end_time )
+                    cout << p.id << " " << p.start_time << " " << p.end_time << " " << STATE_STRING[static_cast<int>(p.status)] << endl;
+                    queue<task> copy(p.tasklist);
+                    while ( copy.empty() == false )
                     {
-                         return false;
+                         cout << "   " << TASK_STRING[static_cast<int>(copy.front().request)] << " " << copy.front().value << endl;
+                         copy.pop();
                     }
-                    if ( t.start_time < p.start_time )
-                    {
-                         return false;
-                    }
-                    if ( t.order != 0 )
-                    {
-                         if ( t.id < p.id )
-                         {
-                              return false;
-                         }
-                    }
-                    p = t;
-                    copy.pop();
                }
-
-               return true;
           }
 
      private:
-          priority_queue<task, vector<task>, task> tasks;
-
-          void
-               time_adjust
-               (
-                    int delay
-               )
-          {
-               // this function is called when a task cannot be executed and must
-               // be delayed until the resource it requires is nextly available
-
-               // to delay a task, 'delay' must be added to its '.start_time' and
-               // '.end_time' | this adjustment will consequently break the ordering
-               // of subsequent tasks | therefore, the '.start_time' and '.end_time'
-               // for each subsequent task with the same '.id' must also be adjusted
-               // with 'delay'
-
-               // here is the process to do so:
-
-               // 1) record the '.id' of the top task
-
-               int id = tasks.top().id;
-
-               // 2) create a temporary task object to store the values of previously
-               //    processed tasks during the operation
-
-               // task { id, request, value, blocking, start_time, end_time, order }
-               task p{ 0, NONE, 0, false, 0, 0, 0 };
-
-               // 3) step through the tasklist adding 'delay' to the '.start_time'
-               //    and '.end_time' of each task with '.id' equal to 'id' | reset
-               //    the '.order' for each task as well, regardless of 'id' | then
-               //    increment the '.order' whenever the task's '.start_time' is
-               //    equal to the previous task's '.start_time'
-
-               // 4) push each task into a temporary priority_queue for obvious
-               //    reasons | after the operation is finished, swap the data of the
-               //    temporary priority queue with the data of the tasklist object
-
-               priority_queue<task, vector<task>, task> temp;
-
-               while ( tasks.empty() == false )
-               {
-                    auto t = tasks.top(); tasks.pop();
-
-                    if ( t.id == id )
-                    {
-                         t.start_time += delay;
-                         t.end_time += delay;
-                    }
-
-                    t.order = 0;
-                    if ( t.start_time == p.start_time )
-                    {
-                         t.order = p.order + 1;
-                    }
-
-                    temp.push(t);
-                    p = t;
-               }
-
-               tasks.swap(temp);
-          }
+          vector<process> processes;
      };
-     */
 
      // input file
      // {NEW,  t} creates new process at time t
@@ -442,6 +192,7 @@ namespace project
      //     return new_table;
      //}
 
+     /// excessively unnecessary class
      class
           builder_task
      {
@@ -519,17 +270,18 @@ namespace project
           void(builder_task::* jump[4])() = { &builder_task::jump_D, &builder_task::jump_I, &builder_task::jump_N, &builder_task::jump_C };
      };
 
+     /// DONE
      class
           builder_tasklist
      {
      public:
-          void
+          static vector<process>
                build_via_jump_table
                (
-                    istream & input_stream,
-                    vector<process> & process_table
+                    istream & input_stream
                )
           {
+               vector<process> process_table;
                builder_task bt;
                tuple<string, int> pair;
 
@@ -554,8 +306,9 @@ namespace project
                     }
                     process_table.back().tasklist.push(t);
                }
+
+               return process_table;
           }
-     private:
           static bool
                next_pair
                (
@@ -576,13 +329,36 @@ namespace project
           }
      };
 
+
      class
-          builder_process
+          machine
      {
      public:
+          machine(ifstream && input_stream)
+          {
+               tuple<string, int> pair;
+               builder_tasklist::next_pair(pair, input_stream);
+               int ncores = get<1>(pair);
+
+               ptable = process_table(builder_tasklist::build_via_jump_table(input_stream));
+               cores = resource_table(ncores);
+               disks = resource_table(1);
+
+               input_stream.close();
+          }
+
+          bool
+               step()
+          {
+               ptable.print();
+               return true;
+          }
 
      private:
-
+          int system_time = 0;
+          process_table ptable;
+          resource_table cores;
+          resource_table disks;
      };
 }
 
@@ -590,12 +366,8 @@ int main(int argc, const char ** argv)
 {
      using namespace project;
 
-     //tasklist t = builder_tasklist::build_via_jump_table(ifstream("1.txt"));
-     //t.execute();
-     //t.print();
-     //cout << ((t.validate() == true) ? "true" : "false") << endl;
-     //t.time_adjust(6);
-     //t.print();
+     machine m(ifstream("1.txt"));
+     m.step();
 
      return 0;
 }
